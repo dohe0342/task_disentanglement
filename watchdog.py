662a664442b9efec183e10c7318ec2585df69376
dohe0342 <kimdohe1070@gmail.com>
2022-03-24 14:30:54 +0900

    from local

---
+++ watchdog.py
@@ -0,0 +1,40 @@
+import os 
+import time 
+
+dirToWatchList = [
+            "./"
+            #"./workspace",
+            #"./scripts/", 
+            #"./scripts/whale/", 
+            #"./scripts/summit/", 
+            #"./fairseq_cli/", 
+            #"./fairseq/", 
+            #"./fairseq/tasks/", 
+            #"./fairseq/optim/",
+            #"./fairseq/criterions/",
+            #"./fairseq/modules/", 
+            #"./fairseq/models/", 
+            #"./fairseq/models/wav2vec/",
+            #"./examples/wav2vec/",
+            #"./examples/wav2vec/config/",
+            #"./examples/wav2vec/config/finetuning/",
+            #"./examples/wav2vec/config/pretraining/",
+            #"./examples/data2vec/",
+            #"./examples/data2vec/models/",
+            #"./examples/data2vec/config/audio/pretraining/",
+            #"./examples/speech_recognition/",
+            #"./examples/speech_recognition/new/",
+            #"./examples/speech_recognition/new/conf/",
+            #"./examples/speech_recognition/new/decoders/"
+            ]
+
+lastmod = [int(os.path.getmtime(dirToWatch)) for dirToWatch in dirToWatchList]
+#lastmod = int(os.path.getmtime(dirToWatch))
+
+while True:
+    for enum, dirToWatch in enumerate(dirToWatchList):
+        if lastmod[enum] != int(os.path.getmtime(dirToWatch)): 
+            #print('Warning: Modify Detected.') 
+            os.system('./git.sh')
+            lastmod[enum] = int(os.path.getmtime(dirToWatch)) 
+    time.sleep(0.5)
